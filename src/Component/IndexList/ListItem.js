0bb6053318d376db6c3934682d2602ee4f67b60b
ckinmind <245160421@qq.com>
2017-02-15 23:53:19 +0800

    将ListItem改为函数式组件，将TabIcon改为TabIcon2，后期会删除TabIcon

---
+++ src/Component/IndexList/ListItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { UserHeadImg, TabIcon2 } from '../common/index';
+import {Tool} from '../../Tool';
+
+const ListItem = ({id, title, author, visit_count, reply_count, create_at, last_reply_at, tab, good, top}) => (
+    <li>
+        <Link to={`/topic/${id}`}>
+            <div data-flex="box:first">
+                <div className="font" data-flex="cross:center">
+                    <TabIcon2 icon={{tab, good, top}}/>
+                </div>
+                <h3 className="tit">{title}</h3>
+            </div>
+            <div className="bottom" data-flex="box:first">
+                <div className="author" data-flex="cross:center">
+                    <UserHeadImg url={author.avatar_url}/>
+                </div>
+                <div className="con" data-flex="dir:top main:center">
+                    <p data-flex="cross:center box:last">
+                        <span className="name">{author.loginname}</span>
+                        <span className="count">{reply_count}/{visit_count}</span>
+                    </p>
+                    <p data-flex="cross:center box:last">
+                        <time className="create">{Tool.formatDate(create_at)}</time>
+                        <time className="re">{Tool.formatDate(last_reply_at)}</time>
+                    </p>
+                </div>
+            </div>
+        </Link>
+    </li>
+);
+
+export default ListItem;
+
