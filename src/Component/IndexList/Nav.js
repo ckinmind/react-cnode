f42b69f6dc7f8495257d1851351e7c926a1b4b33
ckinmind <245160421@qq.com>
2017-02-15 22:50:14 +0800

    更改nav的高亮逻辑，并且修改部分样式，最后将nav转化为函数式组件

--- src/Component/IndexList/Nav.js
+++ src/Component/IndexList/Nav.js
@@ -1,51 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
 
-/**
- * (导航分类)
- *
- * @class Nav
- * @extends {Component}
- *
- * =============================================================
- *  @todo:
- *  1. 这里的高亮方式需要优化，根据route中的属性
- *  2. 高亮的样式应该用active去展示，不要用li中的class on, 这样代码就简化很多了
- *  =============================================================
- */
-class Nav extends Component {
-
-    shouldComponentUpdate(nextProps) {
-        return this.props.tab !== nextProps.tab; //tab和之前的不一致，组件才需要更新，否则不更新，提升性能
-    }
-
-    render() {
-        var setCur = {};
-        setCur[this.props.tab] = 'on';
-        return (
-            <nav className="index-nav">
-                <ul data-flex="box:mean">
-                    <li className={setCur.all}>
-                        <Link to="/" activeClassName="active">全部</Link>
-                    </li>
-                    <li className={setCur.good}>
-                        <Link to="/?tab=good" activeClassName="active">精华</Link>
-                    </li>
-                    <li className={setCur.share}>
-                        <Link to="/?tab=share" activeClassName="active">分享</Link>
-                    </li>
-                    <li className={setCur.ask}>
-                        <Link to="/?tab=ask" activeClassName="active">问答</Link>
-                    </li>
-                    <li className={setCur.job}>
-                        <Link to="/?tab=job" activeClassName="active">招聘</Link>
-                    </li>
-                </ul>
-                <div className="height"></div>
-            </nav>
-        );
-    }
-}
-
+const Nav = ({ tab })=> (
+        <nav className="index-nav">
+            <ul data-flex="box:mean">
+                <li>
+                    <IndexLink to="/" activeClassName={tab == 'all' ? 'on' : ''}>全部</IndexLink>
+                </li>
+                <li>
+                    <Link to="/?tab=good" activeClassName="on">精华</Link>
+                </li>
+                <li>
+                    <Link to="/?tab=share" activeClassName="on">分享</Link>
+                </li>
+                <li>
+                    <Link to="/?tab=ask" activeClassName="on">问答</Link>
+                </li>
+                <li>
+                    <Link to="/?tab=job" activeClassName="on">招聘</Link>
+                </li>
+            </ul>
+            <div className="height"></div>
+        </nav>
+);
 
 export default Nav;
+
+/**
+ *  关于IndexLink高亮的判断：
+ *  因为这里的路由变化的只是？后面的参数，所以如果我不多加一个判断的话，[全部]会一直高亮
+ *  暂时没找到好的解决方案更优雅的方案解决这个问题
+ */
+
