30455af2cd4ef47340b7fda0d47c299dcbea1513
ckinmind <245160421@qq.com>
2017-02-20 02:18:37 +0800

    将tab切换处理从父组件移到Home组件中，简化流程

--- src/Component/UserView/Home.js
+++ src/Component/UserView/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { UserHeadImg } from '../common/index';
 import HomeList from './HomeList';
 import { Tool } from '../../Tool';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 /**
  * 个人主页
@@ -11,11 +11,20 @@ import { Link } from 'react-router';
  * @extends {Component}
  */
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            tabIndex: 0
+        };
+    }
+
     render() {
         let { avatar_url, loginname, score, recent_topics, recent_replies, create_at } = this.props.data;
-        let { tabIndex } = this.props;
+        let arrOn = [];
         let arrDisplay = [];
-        arrDisplay[tabIndex] = 'block';
+        arrOn[this.state.tabIndex] = 'on';
+        arrDisplay[this.state.tabIndex] = 'block';
         return (
             <div className="user-index">
                 <div className="headimg" data-flex="dir:top main:center cross:center">
@@ -24,8 +33,8 @@ class Home extends Component {
                     <div className="score">积分：{score}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;注册于：{Tool.formatDate(create_at)}</div>
                 </div>
                 <ul className="tab-nav" data-flex="box:mean">
-                    <li><Link onClick={() => { this.props.tab(0) } } activeClassName="on">主题</Link></li>
-                    <li><Link onClick={() => { this.props.tab(1) } } activeClassName="on">回复</Link></li>
+                    <li onClick={ ()=>{this.setState({tabIndex:0})} } className={arrOn[0]}>主题</li>
+                    <li onClick={ ()=>{this.setState({tabIndex:1})} } className={arrOn[1]}>回复</li>
                 </ul>
                 <HomeList list={recent_topics} display={arrDisplay[0]} />
                 <HomeList list={recent_replies} display={arrDisplay[1]} />
